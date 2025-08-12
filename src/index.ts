e41b0e0b5bf391dbea32ef8c1c1d88762755b738
Nikhil <vnikhilbuddhavarapu@outlook.com>
2025-08-12 18:40:18 -0400

    fixed CSP headers

--- src/index.ts
+++ src/index.ts
@@ -37,11 +37,11 @@ export default {
       "Content-Security-Policy",
       [
         "default-src 'self'",
-        "script-src 'self' 'unsafe-inline'",
+        "script-src 'self' 'unsafe-inline' https://static.cloudflareinsights.com",
         "style-src 'self' 'unsafe-inline'",
         "img-src 'self' data:",
         "font-src 'self' data:",
-        "connect-src 'self'",
+        "connect-src 'self' https://cloudflareinsights.com https://static.cloudflareinsights.com",
         "object-src 'none'",
         "base-uri 'self'",
         "frame-ancestors 'none'"
